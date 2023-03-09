6312a3b46ecb624c3f382a7ec5dd6d7e18461701
Merlin Eden (THS) <m.eden@ths-bs.de>
2023-03-09 20:29:03 +0100

    [FIR] Replace argument construction for buildResolvedEquals with builder function

--- compiler-plugin/src/main/java/io/mths/kava/compiler/frontend/k2/builder/ResolvedEquals.kt
+++ compiler-plugin/src/main/java/io/mths/kava/compiler/frontend/k2/builder/ResolvedEquals.kt
@@ -6,21 +6,19 @@ import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.expressions.FirEqualityOperatorCall
 import org.jetbrains.kotlin.fir.expressions.FirExpression
 import org.jetbrains.kotlin.fir.expressions.FirOperation
-import org.jetbrains.kotlin.fir.expressions.builder.buildArgumentList
+import org.jetbrains.kotlin.fir.expressions.buildBinaryArgumentList
 import org.jetbrains.kotlin.fir.expressions.builder.buildEqualityOperatorCall
 
 /**
  * Builds a resolved equality operator call which compares
- * [leftOperand] and [rightOperand] using the equals (==) operator.
+ * [left] and [right] using the equals (==) operator.
  */
 context (FirSession)
 fun buildResolvedEquals(
-    leftOperand: FirExpression,
-    rightOperand: FirExpression
+    left: FirExpression,
+    right: FirExpression
 ): FirEqualityOperatorCall =
     buildEqualityOperatorCall {
         operation = FirOperation.EQ
-        argumentList = buildArgumentList {
-            arguments += listOf(leftOperand, rightOperand)
-        }
+        argumentList = buildBinaryArgumentList(left, right)
     } resolvedAs (builtIn { booleanType })
