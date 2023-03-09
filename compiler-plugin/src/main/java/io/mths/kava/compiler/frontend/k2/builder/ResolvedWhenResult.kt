fbb446c03891479768fd6de70806358a1030ac70
Merlin Eden (THS) <m.eden@ths-bs.de>
2023-03-09 16:03:08 +0100

    [FIR] Add method to set resolved result in FirWhenBranchBuilder

---
+++ compiler-plugin/src/main/java/io/mths/kava/compiler/frontend/k2/builder/ResolvedWhenResult.kt
@@ -0,0 +1,16 @@
+package io.mths.kava.compiler.frontend.k2.builder
+
+import io.mths.kava.compiler.frontend.k2.resolve.resolvedAs
+import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.expressions.FirExpression
+import org.jetbrains.kotlin.fir.expressions.builder.FirWhenBranchBuilder
+import org.jetbrains.kotlin.fir.expressions.builder.buildBlock
+
+context (FirSession)
+fun FirWhenBranchBuilder.setResolvedResult(
+    expression: FirExpression
+) {
+    result = buildBlock {
+        statements += expression
+    } resolvedAs expression.typeRef
+}
